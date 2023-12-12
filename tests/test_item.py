f4e7db87fd106ac9b66162c2e332d117d2a7df76
Alexey Elyakin <ieliakin98@mail.ru>
2023-12-12 20:18:53 +0300

    ДЗ_2

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,8 +5,20 @@ from src.item import Item
 def test_calculate_total_price():
     assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
 
+
 def test_apply_discount():
     item = Item("Смартфон", 10000, 20)
     item.pay_rate = 0.5
     item.apply_discount()
     assert item.price == 5000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('7.0') == 7
+    assert Item.string_to_number('5.5') == 5
